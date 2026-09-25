70d4fabd266dd29ed578638002675a373da5b808
Ben <benelderson@gmail.com>
2026-09-25 08:19:05 -0400

    completed each function in index.js according to the jsdoc

--- index.js
+++ index.js
@@ -12,6 +12,11 @@
  */
 export function echo(word, n) {
   // TODO
+  let result = "";
+  for (let i = 0; i < n; i++) {
+    result += word;
+  }
+  return result;
 }
 
 /**
@@ -28,6 +33,15 @@ export function echo(word, n) {
  */
 export function echoWithSpace(word, n) {
   // TODO
+  if (n <= 0) return "";
+  if (word.length === 0) return "";
+
+  let result = "";
+  for (let i = 1; i < n; i++) {
+    result += word + " ";
+  }
+  result += word;
+  return result;
 }
 
 /**
@@ -43,6 +57,11 @@ export function echoWithSpace(word, n) {
  */
 export function sumTo(n) {
   // TODO
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    result += i;
+  }
+  return result;
 }
 
 /**
@@ -59,6 +78,11 @@ export function sumTo(n) {
  */
 export function sumFromTo(a, z) {
   // TODO
+  let result = 0;
+  for (let i = a; i <= z; i++) {
+    result += i;
+  }
+  return result;
 }
 
 /**
@@ -74,6 +98,9 @@ export function sumFromTo(a, z) {
  */
 export function countdown(n) {
   // TODO
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
 }
 
 /**
@@ -89,6 +116,13 @@ export function countdown(n) {
  */
 export function sumOddsToN(n) {
   // TODO
+  let result = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 !== 0) {
+      result += i;
+    }
+  }
+  return resul;
 }
 
 /**
@@ -109,6 +143,14 @@ export function sumOddsToN(n) {
  */
 export function getGrowthTime(start, target) {
   // TODO
+  if (start <= 0) return undefined;
+
+  let i = 0;
+  while (start < target) {
+    start *= 2;
+    i += 1;
+  }
+  return i * 20;
 }
 
 /**
@@ -129,6 +171,14 @@ export function getGrowthTime(start, target) {
  */
 export function getCompoundTime(start, rate, target) {
   // TODO
+  if (start <= 0 || rate <= 0) return undefined;
+
+  let i = 0;
+  while (start < target) {
+    start *= 1 + rate;
+    i += 1;
+  }
+  return i;
 }
 
 /**
@@ -155,6 +205,19 @@ export function getCompoundTime(start, rate, target) {
  */
 export function moveWater(colander, bucket) {
   // TODO
+  if (colander <= 0) return undefined;
+  if (bucket <= 0) return 0;
+  if (colander >= bucket) return 1;
+  if (colander === 1) return bucket;
+
+  let current = 0;
+  let trips = 0;
+  while (current < bucket) {
+    current += colander;
+    colander = Math.max(1, colander - 1);
+    trips += 1;
+  }
+  return trips;
 }
 
 /**
@@ -175,4 +238,15 @@ export function moveWater(colander, bucket) {
  */
 export function fizzbuzz(n) {
   // TODO
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
 }
